4eaccd2ebab100097dc29cab9f308a4ce34968c3
MattrExp <108210298+MattrExp@users.noreply.github.com>
2022-07-13 11:17:57 +0530

    Update model-viewer.js

--- model-viewer.js
+++ model-viewer.js
@@ -175,8 +175,8 @@ AFRAME.registerComponent('model-viewer', {
       backgroundEl.setAttribute('geometry', {primitive: 'sphere', radius: 65});
       backgroundEl.setAttribute('material', {
         shader: 'background-gradient',
-        colorTop: '#141414',
-        colorBottom: '#141414',
+        colorTop: '##dbe0e4',
+        colorBottom: '##dbe0e4',
         side: 'back'
       });
       backgroundEl.setAttribute('hide-on-enter-ar', '');
